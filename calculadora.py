ac9b363c71dcbdbd15704c9032ef5efab735ae80
mhelynne <mariahelynne@gmail.com>
2020-11-28 09:41:59 -0300

    primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,23 @@
+def soma(a, b):
+	# implementar função
+
+def subtrai(a, b):
+	# implementar função
+
+def multiplica(a, b):
+	# implementar função
+
+def divide(a, b):
+	# implementar função
+
+#Programa principal
+
+print("Calculadora simples")
+
+num1 = float(input("Insira o primeiro numero: "))
+num2 = float(input("Insira o segundo numero: "))
+
+soma(num1, num2)
+subtrai(num1, num2)
+multiplica(num1, num2)
+divide(num1, num2)
